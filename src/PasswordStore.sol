cd9ec8590de6e130eb02a828002779cfecbd1eec
Nihavent <ntaylor2494@gmail.com>
2024-01-11 22:31:44 +1000

    audit notes

--- src/PasswordStore.sol
+++ src/PasswordStore.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.18;
+pragma solidity 0.8.18; //q is this the latest version?
 
 /*
  * @author not-so-secure-dev
@@ -11,6 +11,7 @@ contract PasswordStore {
     error PasswordStore__NotOwner();
 
     address private s_owner;
+    // @audit the s_password variable is not private, this is not a safe place to store your password
     string private s_password;
 
     event SetNetPassword();
@@ -23,6 +24,9 @@ contract PasswordStore {
      * @notice This function allows only the owner to set a new password.
      * @param newPassword The new password to set.
      */
+    // q can a non-owner set the password?
+    // q should a non-owner be able to set the password?
+    // @audit any user can set the password - missing access control
     function setPassword(string memory newPassword) external {
         s_password = newPassword;
         emit SetNetPassword();
@@ -30,6 +34,7 @@ contract PasswordStore {
 
     /*
      * @notice This allows only the owner to retrieve the password.
+     //@audit there is no newPassword parameter
      * @param newPassword The new password to set.
      */
     function getPassword() external view returns (string memory) {
